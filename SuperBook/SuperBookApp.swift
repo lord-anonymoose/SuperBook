70511b7ea81b6814321061db229dc7e0a4804ebc
Philipp Lazarev <philipp.lazareff@gmail.com>
2024-08-16 21:59:02 +0300

    Initial commit for SuperBook App

---
+++ SuperBook/SuperBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperBookApp.swift
+//  SuperBook
+//
+//  Created by Philipp Lazarev on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
