11f67ae5d4c92fef0cb537a1f57ff077da09b8f4
Philipp Lazarev <philipp.lazareff@gmail.com>
2024-08-28 21:51:41 +0300

    Added TabView and App Icon

--- SuperBook/ContentView.swift
+++ SuperBook/ContentView.swift
@@ -8,10 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @AppStorage("tab") private var tabSelected = 1
     @State private var heroes: [Superhero] = []
     
     var body: some View {
-        SuperheroList(heroes: self.heroes)
+        TabView(selection: $tabSelected) {
+            SuperheroList(heroes: heroes)
+                .tabItem {
+                    Label("Superheroes", systemImage: "list.star")
+                }
+                .tag(1)
+            
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
+                .tag(2)
+        }
     }
 }
 
